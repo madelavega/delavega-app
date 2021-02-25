b0a3a4e5cd450231cdd43921eaeb58f307700182
Manu <manuelangel.delavega.next@bbva.com>
2021-02-25 17:34:24 +0100

    service rootpath

--- config/production/services.js
+++ config/production/services.js
@@ -1,4 +1,5 @@
-const ROOT = '/mockups';
+
+const ROOT =  require('./environment').ROOT_PATH + '/mockups';
 module.exports = {
     // euroJackpotResults : 'https://media.lottoland.com/api/drawings/euroJackpotn/{date}',
     euroJackpotResults : `${ROOT}/euroJackpotResults.json?{date}`
